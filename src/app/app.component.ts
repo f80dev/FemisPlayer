b0fa59c7731d901228ce95c01bb719fb5cc77943
f80dev <herve.hoareau@f80.fr>
2021-05-04 14:43:38 +0200

    version de test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,9 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./app.component.sass']
 })
 
-export class AppComponent implements OnInit {
-  title = 'player';
+export class AppComponent {
 
   constructor(
-    public router:Router,
-    public routes:ActivatedRoute
   ) { }
 
-  ngOnInit(): void {
-    setTimeout(()=>{
-      let department=this.routes.snapshot.queryParamMap.get('department') || '*';
-      let year=this.routes.snapshot.queryParamMap.get('year') || 0;
-    },100);
-  }
 }
