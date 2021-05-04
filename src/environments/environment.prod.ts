e2ac10bedb9afd3cbf435a0a25094097b9532d64
f80dev <herve.hoareau@f80.fr>
2021-05-04 17:17:57 +0200

    version de test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   domain_server:"",
-  version:'0.2',
+  version:'0.3',
   film_catalogue:'https://femisplayer.f80.fr/assets/films.csv',
   domain_appli: 'https://femisplayer.f80.fr'
 };
