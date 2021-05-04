79456c4f4881f6cf314484eea712f5b6fba49348
f80dev <herve.hoareau@f80.fr>
2021-05-04 17:28:34 +0200

    version de test

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -20,37 +20,44 @@ export class FilmsComponent implements OnInit {
     public routes:ActivatedRoute
   ) { }
 
-  ngOnInit(): void {
-
-    let params=this.routes.snapshot.paramMap.get("p");
+  refresh(){
+    let params=this.routes.snapshot.queryParamMap.get("p");
     let decod_params=atob(params).split(",");
 
     let department=decod_params[1];
     let year=decod_params[2];
+    debugger
 
     if(department && year){
       this.api._get("../assets/films.json").subscribe((r:any)=>{
-      this.films=[];
-      for(let film of r){
-        let film_year=film.promo-new Date().getFullYear();
-        if ((film.department.toLowerCase()==department.toLowerCase() || department=="*") && (film_year==Number(year) || year=='0')){
-          if(film.videoId.startsWith('vimeo')){
-            film.videoId=film.videoId.replace("vimeo","");
-            film.iframe_url=null;
-            film.fullscreen_url="https://player.vimeo.com/video/"+film.videoId;
-          }else{
-            film.iframe_url='https://embed.api.video/vod/'+film.videoId
-            film.fullscreen_url="https://embed.api.video/vod/"+film.videoId;
+        this.films=[];
+        if(r.length==0){
+          this.message="Aucun film disponible de "+year+" année en section "+department;
+        } else {
+          for(let film of r){
+            let film_year=film.promo-new Date().getFullYear();
+            if ((film.department.toLowerCase()==department.toLowerCase() || department=="*") && (film_year==Number(year) || year=='0')){
+              if(film.videoId.startsWith('vimeo')){
+                film.videoId=film.videoId.replace("vimeo","");
+                film.iframe_url=null;
+                film.fullscreen_url="https://player.vimeo.com/video/"+film.videoId;
+              }else{
+                film.iframe_url='https://embed.api.video/vod/'+film.videoId
+                film.fullscreen_url="https://embed.api.video/vod/"+film.videoId;
+              }
+              this.films.push(film);
+            }
           }
-          this.films.push(film);
         }
-
-      }
-    })
+      })
     } else {
       this.message="Vous devez obtenir un lien de la FEMIS pour consulter les films";
     }
 
+  }
+
+  ngOnInit(): void {
+    setTimeout(()=>{this.refresh()},500);
   }
 
 }
